1e1e8e0e713f4a9247acd0904177411781dd86da
JAVIER MARTIN PIZARRO <jmartinpizarro04@gmail.com>
2025-04-22 11:17:33 +0200

    feat: update url

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -13,7 +13,7 @@ function LogIn() {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch('http://localhost:8000/api/login', {
+            const response = await fetch('http://localhost:8000/api/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
